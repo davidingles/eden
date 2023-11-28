6006ff98df8f1e6db8bea9e31986fd8e9d70ef7c
davidingles <davidingleslopez@gmail.com>
2023-11-28 13:35:10 +0100

    personalizo clases de tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			borderWidth: {
+				'miBorde': '10px',
+				borderColor: {
+					'miColor': '#ff0000',
+				}
+			}
+
+
+		},
+	},
+	variants: {
+
 	},
 	plugins: [],
 }
